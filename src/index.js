769d804dcdcdbe338eaff1e651d47accc68fd817
Alexrsnchz <alexrsnchz@gmail.com>
2025-08-26 01:31:16 +0200

    chore: Add cors middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { corsMiddleware } from './middleware/corsMiddleware.js';
 import { categoriesRouter } from './routes/categoriesRouter.js';
 import { apisRouter } from './routes/apisRouter.js';
 import { errorHandler } from './middleware/errorHandler.js';
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.disable('x-powered-by');
 app.use(express.json());
+app.use(corsMiddleware);
 
 app.use('/api/categories', categoriesRouter);
 app.use('/api/apis', apisRouter);
